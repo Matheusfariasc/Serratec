64d35e90a39a252f4df7fff22e8bb60e2d25f025
Matheusfariasc <81425135+Matheusfariasc@users.noreply.github.com>
2021-04-09 23:22:31 -0300

    Add files via upload

---
+++ Fase 2/Indeciso.js
@@ -0,0 +1,13 @@
+//O programa "indeciso" não sabe bem o número que quer imprimir.
+
+let sorte 
+let sorte2 = 22
+
+sorte = 13
+console.log("Te contarei o meu número da sorte,que é " + sorte + " !");
+
+sorte = 7 
+console.log("Não, espera. É " + sorte + ".. Ah não, esse era o do meu irmão. O meu é " + sorte2 + " !");
+
+sorte = 20 
+console.log("Já decidi! É " + sorte + " BINGO! ");
